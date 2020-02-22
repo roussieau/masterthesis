fc0ab0be0b9b9dac3058dc742bd70119388a159b
Jereminet <jeremy.minet@student.uclouvain.be>
2020-02-22 19:06:26 +0100

    only agreed packers

--- src/scripts/csv_dump.py
+++ src/scripts/csv_dump.py
@@ -9,10 +9,11 @@ db = Database()
 from_date = sys.argv[1]
 to_date = sys.argv[2]
 threshold = sys.argv[3]
+agreement = sys.argv[4]
 dt_array = []
 interval = db.getDates(from_date, to_date)
 for x in interval:
-	dt_array.append(db.getGroundTruth(x, threshold))
+	dt_array.append(db.getGroundTruth(x, int(threshold), int(agreement)))
 now = datetime.now()
 timestamp = now.strftime("%Y.%m.%d-%H.%M")
 gt = pd.concat(dt_array)
