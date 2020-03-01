6b3dc0c18133da6a5f53623ad01c4a0bbaee738b
Jereminet <jeremy.minet@student.uclouvain.be>
2020-03-01 14:49:24 +0100

    ml testing + some dumps

---
+++ src/machine_learning/KNeighbors.py
@@ -0,0 +1,43 @@
+import sys
+import argparse
+import time
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def display_plot(csv, t_size, max_neigh):
+	gt = pd.read_csv(csv)
+	cols = [col for col in gt.columns if col not in ['label']]
+	data = gt[cols]
+	target = gt['label']
+
+	data_train, data_test, target_train, target_test = train_test_split(data,target, test_size = t_size, random_state = 0)
+
+	training_accuracy = [] 
+	test_accuracy = []
+	neighbors_settings = range(1, max_neigh)
+	for n_neighbors in neighbors_settings:
+	    clf = KNeighborsClassifier(n_neighbors=n_neighbors) 
+	    clf.fit(data_train, target_train)
+	    training_accuracy.append(clf.score(data_train, target_train))
+	    test_accuracy.append(clf.score(data_test, target_test))
+	plt.plot(neighbors_settings, training_accuracy, label="training accuracy") 
+	plt.plot(neighbors_settings, test_accuracy, label="test accuracy") 
+	plt.ylabel("Accuracy")
+	plt.xlabel("n_neighbors")
+	plt.legend()
+
+def sober_results(csv, t_size, neigh):
+	gt = pd.read_csv(csv)
+	cols = [col for col in gt.columns if col not in ['label']]
+	data = gt[cols]
+	target = gt['label']
+
+	data_train, data_test, target_train, target_test = train_test_split(data,target, test_size = t_size, random_state = 0)
+
+	clf = KNeighborsClassifier(n_neighbors=neigh)
+	clf.fit(data_train, target_train)
+	print("Training set accuracy: {:.2f}".format(clf.score(data_train, target_train)))
+	print("Test set accuracy: {:.2f}".format(clf.score(data_test, target_test)))
