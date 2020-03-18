09558075508cf27ba8a944355bc4e7eca6e2da84
Julian Roussieau <roussieau.j@gmail.com>
2020-03-18 10:02:49 +0100

    create bin folder

---
+++ src/detector/bin/peid/peid.py
@@ -0,0 +1,18 @@
+import peutils
+import pefile
+
+def peidAnalysis(malware):
+    with open('./peid/db_signatures.txt', encoding="ISO-8859-1",) as f: 
+        sig_data = f.read()
+        signatures = peutils.SignatureDatabase(data=sig_data)
+        malwarePath = "{}/{}".format(malware.path, malware.name)
+
+    try:
+        pe = pefile.PE(malwarePath)
+        matches = signatures.match(pe, ep_only = True)
+        return matches
+    except Exception as e:
+        return 'error'
+
+
+
