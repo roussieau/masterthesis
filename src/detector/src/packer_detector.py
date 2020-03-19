16fe38c0a64d6b8320d93940b1b8257d89b4898a
Julian Roussieau <roussieau.j@gmail.com>
2020-03-19 11:53:19 +0100

    add method compute_and_save()

--- src/detector/src/packer_detector.py
+++ src/detector/src/packer_detector.py
@@ -18,9 +18,13 @@ class PackerDetector:
     def compute_and_save(self, show=False):
         results = self.analyze()
         if show:
-            print(results)
-        for result in results: 
-            db.add_analysis(self.malware.get_id(), self.get_id(), result)
+            print("{}: {}".format(self.get_detector_name(), results))
+
+        if type(results) is list:
+            for result in results: 
+                db.add_analysis(self.malware.get_id(), self.get_id(), result)
+        else: 
+            db.add_analysis(self.malware.get_id(), self.get_id(), results)
 
     def __str__(self):
         result = self.analyze()
