93477e8e917180086fb81306f8cf13d5f169ebcb
Julian Roussieau <roussieau.j@gmail.com>
2020-03-21 15:27:02 +0100

    add smart file

---
+++ src/detector/src/smart.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import subprocess
+from database import Database
+
+THRESHOLD_MALWARE = 900
+THRESHOLD_FEATURE = THRESHOLD_MALWARE * 119
+
+db = Database()
+
+def convert_date(zipped_date):
+    year, month, day = zipped_date.split(".")[0].split("-")
+    return "{}{}{}".format(year, month, day)
+
+def is_zip(f):
+    splited = f.split('.')
+    return len(splited) == 2 and splited[1] == "zip"
+
+def gen_list(list_of_files):
+    for f in list_of_files:
+        if is_zip(f):
+            yield (convert_date(f), f)
+
+def need_to_scan(date):
+    return db.get_number_of_detections(date) < THRESHOLD_MALWARE or \
+        db.get_number_of_feature_values(date) < THRESHOLD_FEATURE
+
+def scan(date, f):
+    zipped_path = "./malwares/{}".format(f)
+    new_folder = "./malwares/{}".format(date)
+    subprocess.run(["scp", "shadow:~/malware/{}".format(f), zipped_path])
+    subprocess.run(["unzip", zipped_path, "-d", new_folder])
+    subprocess.run(["rm", "-r", zipped_path]) 
+    subprocess.run(["./detector.py", "./malwares", "--verbose", "--auto"])
+    subprocess.run(["rm", "-r", new_folder]) 
+    
+
+def main():
+    try:
+        list_of_files = subprocess.check_output(["ssh", "shadow",
+            "ls", "malware"]).decode("utf-8").split("\n")
+    except:
+        print("Error to connect in ssh")
+
+    folders = list(gen_list(list_of_files))
+    for (date, f) in folders:
+        if need_to_scan(date):
+            scan(date, f)
+
+if __name__ == '__main__':
+    main()
