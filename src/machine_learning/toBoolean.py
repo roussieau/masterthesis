5fa7f3e8b6f5caef5f263473cb746889304c7712
Julian Roussieau <roussieau.j@gmail.com>
2020-03-29 18:23:52 +0200

    convert all features

--- src/machine_learning/toBoolean.py
+++ src/machine_learning/toBoolean.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 class BufferDF:
-    
     def __init__(self):
         self.df = pd.DataFrame()
 
@@ -22,11 +21,23 @@ class BufferDF:
 def do_nothing(series):
     return series
 
-def convert_f9(series):
-    return series.apply(lambda x: 0 if x == 0 else 1)
+def delete(series):
+    return None
+
+def check_zero(series):
+    return series.apply(lambda x: 1 if x == 0 else 0)
 
 def convert_f10(series):
     return series.apply(lambda x: 1 if x == 4_194_304 else 0)
+
+def check_ratio(series):
+    def convert(x):
+        if x > 1:
+            return 1
+        elif x < 1:
+            return 2 
+        return 0
+    return series.apply(convert)
 
 def convert_f11(series):
     def convert(x):
@@ -46,9 +57,6 @@ def convert_f12(series):
         return 0
     return series.apply(convert)
 
-def convert_f13(series):
-    return series.apply(lambda x: 1 if x == 0 else 0)
-
 def convert_f14(series):
     return series.apply(lambda x: 1 if x < 250000 else 0)
 
@@ -67,13 +75,6 @@ def convert_f16(series):
             return 4
         return 0
     return series.apply(convert)
-
-def convert_f17(series):
-    return None
-
-def convert_f18(series):
-    return series.apply(lambda x: 1 if x == 0 else 0)
-
 
 def convert_f19(series):
     return series.apply(lambda x: 1 if x == 1048576 else 0)
@@ -127,36 +128,14 @@ def convert_f29(series):
 def convert_f30(series):
     return series.apply(lambda x: x if x < 3 else 3)
 
-def convert_f37(series):
-    def convert(x):
-        if x > 0:
-            return 1
-        elif x < 0:
-            return -1
-        return 0
-    return series.apply(convert)
-
 def convert_f38(series):
     return series.apply(lambda x: x if x < 3 else 3)
 
 def convert_f39(series):
     return series.apply(lambda x: x if x < 4 else 4)
 
-def convert_f40(series):
-    def convert(x):
-        if x > 1:
-            return 1
-        elif x < 1:
-            return 2 
-        return 0
-    return series.apply(convert)
-
-def convert_f41(series):
-    return series.apply(lambda x: 1 if x == 0 else 0)
-
-def convert_f43(series):
+def check_negative(series):
     return series.apply(lambda x: 1 if x == -1 else 0)
-
 
 
 functions = [
@@ -169,16 +148,16 @@ functions = [
         do_nothing, #f6
         do_nothing, #f7
         do_nothing, #f8
-        convert_f9,
+        check_zero, #f9
         convert_f10,
         convert_f11,
         convert_f12,
-        convert_f13,
+        check_zero, #f13
         convert_f14,
         convert_f15,
         convert_f16,
-        convert_f17,
-        convert_f18,
+        delete, # f17
+        check_zero, # f18
         convert_f19,
         convert_f20,
         convert_f21,
@@ -197,21 +176,25 @@ functions = [
         do_nothing, #f34
         do_nothing, #f35
         do_nothing, #f36
-        convert_f37,
+        check_ratio, #f37
         convert_f38,
         convert_f39,
-        convert_f40,
-        convert_f41,
-        do_nothing,
-        convert_f43,
+        check_ratio, #f40
+        check_zero, #f41
+        do_nothing, #f42
+        check_negative, #43
+        check_negative,#f44
+        check_negative,#f45
 ]
 
 def convert(df):
     buff = BufferDF()
-    for i in range(1,43):
+    for i in range(1,46):
         feature = 'f{}'.format(i)
         print("for {} we use {}".format(feature, functions[i]))
         buff.add(feature, functions[i](df[feature]))
 
-    print(buff.get_df())
+    # We delete features 46 to 117
+    buff.add('f118', df['f118'])
 
+    return buff.get_df()
