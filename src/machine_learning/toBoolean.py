45b59e7b68eaa85354648ba827d4f2871da023fa
Jereminet <jeremy.minet@student.uclouvain.be>
2020-04-01 10:48:12 +0200

    opti convert

--- src/machine_learning/toBoolean.py
+++ src/machine_learning/toBoolean.py
@@ -188,40 +188,23 @@ functions = [
         check_negative,#f45
 ]
 
-def convert(df):
+def convert(df,all_features):
     buff = BufferDF()
     for i in range(1,46):
         feature = 'f{}'.format(i)
         #print("for {} we use {}".format(feature, functions[i]))
         buff.add(feature, functions[i](df[feature]))
 
-    for i in range(46,118):
-        feature = 'f{}'.format(i)
-        #print("for {} we use {}".format(feature, check_zero))
-        buff.add(feature, check_zero(df[feature]))
+    if all_features:
+        for i in range(46,118):
+            feature = 'f{}'.format(i)
+            #print("for {} we use {}".format(feature, check_zero))
+            buff.add(feature, check_zero(df[feature]))
 
     # We delete features 46 to 117
     buff.add('f118', df['f118'])
 
-    buff.add('f119', check_zero(df['f119']))
-
-    return buff.get_df()
-
-def convert_only_bool(df):
-    buff = BufferDF()
-    for i in range(1,46):
-        feature = 'f{}'.format(i)
-        #print("for {} we use {}".format(feature, functions[i]))
-        buff.add(feature, functions[i](df[feature]))
-
-    '''for i in range(46,118):
-        feature = 'f{}'.format(i)
-        #print("for {} we use {}".format(feature, check_zero))
-        buff.add(feature, check_zero(df[feature]))'''
-
-    # We delete features 46 to 117
-    buff.add('f118', df['f118'])
-
-    #buff.add('f119', check_zero(df['f119']))
+    if all_features:
+        buff.add('f119', check_zero(df['f119']))
 
     return buff.get_df()
