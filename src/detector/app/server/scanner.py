c7ccd560f5de799bcc106219895033ec364de33a
Jereminet <jeremy.minet@student.uclouvain.be>
2020-06-08 20:25:36 +0200

    scanner fixed

--- src/detector/app/server/scanner.py
+++ src/detector/app/server/scanner.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 import werkzeug, sys, os
 
-sys.path.append('../src')
+sys.path.append('../../src')
 from pefeats import Pefeats
 from detector import Malware
 
